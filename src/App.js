55261251d0e2c3fbc008b10dc49c688e1559c9ae
Tom Pakeman <tompakeman@google.com>
2022-11-16 12:49:13 +0100

    Fixed proxy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
-import React from "react";
-import { ComponentsProvider } from '@looker/components';
+import React, { useEffect, useState, createRef } from "react";
+import { Button, ComponentsProvider, Header } from '@looker/components';
 import { Spinner } from '@looker/components';
+import { LookerEmbedSDK } from '@looker/embed-sdk';
 
 
-const LoadingElement = ({size}) => {
-    return (
-        <Spinner size={size}></Spinner>
-    )
-}
+const LoadingElement = ({size=40}) => <div className='maxSize'><Spinner size={size}/></div>
+LookerEmbedSDK.init(process.env.LOOKER_HOST, '/api/sso_auth')
 
 const App = () => {
+    const [fullScreen, setFullScreen] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [dashboard, setDashboard] = useState()
+    const dashRef = createRef()
+    const dashboardTitle = 'Looker Dashboard'
+    useEffect(() => {
+        LookerEmbedSDK.createDashboardWithId(11)
+        .appendTo(dashRef)
+        .build()
+        .connect()
+        .then()
+        .catch((error: Error) => {
+          console.error('An unexpected error occurred', error)
+        })
+    }, [dashRef])
+
     return (
         <ComponentsProvider>
-            <div>
-                <div>
-                    Hi                
+            <div className='app'>
+                <Header as='h2'>{dashboardTitle}</Header>
+                <div className={`dashboard ${fullScreen ? 'fullScreenOverlay' : ''}`}>
+                    <Button onClick={() => setFullScreen(!fullScreen)}>
+                        Toggle fullscreen
+                    </Button>
+                    {isLoading 
+                    ? <LoadingElement size={40}/>
+                    : <></>
+                }
                 </div>
             </div>
       </ComponentsProvider>
